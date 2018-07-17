728ec683353e0a174d26cc3c362d66c3c3bf9732
Nicolai Garcia <nicogarcia@Nicolais-MacBook-Pro.local>
2018-07-16 21:54:42 -0700

    Parse server connection successful.

--- Q/Q/AppDelegate.swift
+++ Q/Q/AppDelegate.swift
@@ -21,9 +21,25 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
             $0.server = "http://q-server-nicolaig.herokuapp.com/parse"
         }
         Parse.initialize(with: configuration)
+        testServer()
         return true
     }
 
+    func testServer() {
+        var gameScore = PFObject(className:"GameScore")
+        gameScore["score"] = 1337
+        gameScore["playerName"] = "Sean Plott"
+        gameScore["cheatMode"] = false
+        gameScore.saveInBackground {
+            (success: Bool, error: Error?) in
+            if (success) {
+                print("SUCCESSFULLY STORED!")
+            } else {
+                print("ERROR NOT STORED!")
+            }
+        }
+    }
+    
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
